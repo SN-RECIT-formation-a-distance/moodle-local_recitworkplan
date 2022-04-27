5034789ec7a38f4d44a245817545a8826cf485d1
Gustavo Bazzo <gcbazzo@gmail.com>
2022-04-27 14:23:56 -0300

    Update version.php

--- src/version.php
+++ src/version.php
@@ -22,7 +22,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2022020907;       // The current module version (Date: YYYYMMDDXX)
-$plugin->release = 'v1.0.4';
+$plugin->release = 'v1.0.5';
 $plugin->requires  = 2020061500.00; // Moodle 3.9.0
 $plugin->component = 'local_recitworkplan';        // Full name of the plugin (used for diagnostics)
 $plugin->cron      = 0;
